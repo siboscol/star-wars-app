6ffc801dcced17bae0c35ffc2915be6a90080ee4
Simone Boscolo Berto <simone.bb2003@gmail.com>
2022-01-02 18:24:40 +0100

    Fist commit

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import App from './App'
+import theme from './theme'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
+  document.querySelector('#root')
+)
