047102013226be8b6780784a3801b70d5074db55
Simone Boscolo Berto <simone.bb2003@gmail.com>
2022-01-08 00:15:06 +0100

    Improved hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,27 +1,30 @@
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Box, Container, Typography } from '@mui/material'
 
 export default function Hero() {
   return (
     <Box
       sx={{
         bgcolor: 'background.paper',
-        pt: 8,
+        pt: 6,
         pb: 6
       }}
     >
       <Container maxWidth="sm">
         <Typography component="h1" variant="h2" align="center" color="text.primary" gutterBottom>
-          Album layout
+          Star Wars
         </Typography>
         <Typography variant="h5" align="center" color="text.secondary" paragraph>
-          Something short and leading about the collection below—its contents, the creator, etc.
-          Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-          entirely.
+          All the Star Wars data you've ever wanted
         </Typography>
-        <Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent="center">
-          <Button variant="contained">Main call to action</Button>
-          <Button variant="outlined">Secondary action</Button>
-        </Stack>
+        <Typography variant="h6" align="center" color="text.secondary" paragraph>
+          Planets, Spaceships, Vehicles, People, Films and Species
+        </Typography>
+        <Typography variant="h6" align="center" color="text.secondary" paragraph>
+          From all SEVEN Star Wars films Now with The Force Awakens data!
+        </Typography>
+        <Typography variant="h4" align="center" color="text.secondary" paragraph>
+          Now with The Force Awakens data!
+        </Typography>
       </Container>
     </Box>
   )
